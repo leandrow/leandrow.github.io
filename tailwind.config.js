b83d276f64710ffe36b4eb5e2d444dc3bdd1710c
Leandro Silva <leandrow01s@gmail.com>
2023-12-24 18:40:39 -0300

    define dark mode as default

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 export default {
   content: ['./dev/**/*.{html,js}'],
-  darkMode: 'media',
+  darkMode: 'class',
   theme: {
     screens: {
       sm: '480px',
